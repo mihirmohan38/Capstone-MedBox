8b15761c976db33a4f35d6879b7c237725d860be
mihirmohan38 <mihirmohan38@gmail.com>
2021-07-15 11:09:29 +0800

    mihir chore : fixed schema for med_logs and added addLog endpoint

--- backend/logs.js
+++ backend/logs.js
@@ -18,7 +18,7 @@ logsRouter.use((req, res, next) => {
 }) ; 
 
 /*
-/logs
+/logs/getLogs
 header {jwt : token}
 body {
     username : username 
@@ -26,7 +26,7 @@ body {
 }
 */
 
-logsRouter.get('/', (req, res)=>{
+logsRouter.post('/getLogs', (req, res)=>{
     var medboxID = req.body.medboxID ;  
     pool.query(`SELECT * FROM med_logs WHERE medbox_id=? LIMIT 10 `,medboxID, function(error, result){
         if(error) {
@@ -37,6 +37,45 @@ logsRouter.get('/', (req, res)=>{
         }
     }) ;  
 }) ;  
+
+
+
+/*
+/logs/addLog
+header {jwt : token}
+body {
+    username : username , 
+    medboxID : medboxID, 
+    medicineID : medicineID , 
+    medicineName : medicineName , 
+    quantity : quantity of medicine 
+    taken : [0 or 1] boolen value 
+    timeSlot : either of ["M", "L", "E", "D"] for the four timeslots
+    timesSnoozed : number of time medicine taking was snoozed
+}
+
+
+*/
+
+logsRouter.post("/addLog", (req, res)=>{
+    var medboxID = req.body.medboxID ; 
+    var medicineID = req.body.medicineID ; 
+    var medicineName = req.body.medicineName ; 
+    var quantity = req.body.quantity ; 
+    var taken = req.body.taken ; 
+    var timeSlot= req.body.timeSlot ; 
+    var timesSnoozed = req.body.timesSnoozed ; 
+       
+    var query = "INSERT INTO med_logs (medbox_id, medicine_id, medicine_name, quantity, taken, time_slot, time_taken, times_snoozed) VALUES  (?,?,?,?, ?, ?, sysdate(), ?) "
+    var values = [medboxID, medicineID, medicineName, quantity, taken, timeSlot, timesSnoozed] ; 
+    pool.query(query, values, function(error, result){
+        if(error){
+            res.json({'success': 0, 'error' : 'db connection error', 'data': null}) ;
+        } else {
+            res.json({'success': 1, 'error' : null, 'data': null }) ;
+        }
+    }) ; 
+}) ; 
 
     
 
